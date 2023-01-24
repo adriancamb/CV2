3e5cf3c81f12a10396609a0279af9ab5e1581823
Adrián Cambres <adriancamb@yahoo.com>
2023-01-24 14:59:05 -0300

    Version 1.5 prueba celular 3 opciones de ancho de pantalla

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 function funcionResize() {
     $('#widthval').html($(document).width());        
-        if ($(window).width() < 800) {
+        if ($(window).width() < 768) {
             $('.container-fluid').css('padding-left', '5%');
             $('.container-fluid').css('padding-right', '8%');
+            //console.log("768");
         }
-        else if ($(window).width() < 1700) {
+        else if ($(window).width() < 1600 && ($(window).width() >= 768)) {
             $('.container-fluid').css('padding-left', '5%');
             $('.container-fluid').css('padding-right', '0%');
+            //console.log("1600");
         }
         else
         {
             $('.container-fluid').css('padding-left', '15%');
             $('.container-fluid').css('padding-right', '10%');
+            //console.log("else");
         }
     }
 
