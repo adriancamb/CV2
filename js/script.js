8b73ce6587b845230da43af07ab785b0610d28a0
Adrián Cambres <adriancamb@yahoo.com>
2023-02-02 19:44:27 -0300

    Fecha de último ingreso

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 function funcionResize() {
     $('#widthval').html($(document).width());        
-        if ($(window).width() < 768) {
-            $('.container-fluid').css('padding-left', '5%');
-            $('.container-fluid').css('padding-right', '8%');
-            //console.log("768");
-        }
-        else if ($(window).width() < 1600 ) {
-            $('.container-fluid').css('padding-left', '5%');
-            $('.container-fluid').css('padding-right', '0%');
-            //console.log("1600");
-        }
-        else
-        {
-            $('.container-fluid').css('padding-left', '15%');
-            $('.container-fluid').css('padding-right', '10%');
-            //console.log("else");
-        }
+    if ($(window).width() < 768) {
+        $('.container-fluid').css('padding-left', '5%');
+        $('.container-fluid').css('padding-right', '8%');
+        //console.log("768");
     }
+    else if ($(window).width() < 1600 ) {
+        $('.container-fluid').css('padding-left', '5%');
+        $('.container-fluid').css('padding-right', '0%');
+        //console.log("1600");
+    }
+    else
+    {
+        $('.container-fluid').css('padding-left', '15%');
+        $('.container-fluid').css('padding-right', '10%');
+        //console.log("else");
+    }
+}
 
 $(window).on("resize", funcionResize);
